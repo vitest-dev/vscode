75d240f90a9853b82b04c3f50e691da9cd5875de
Zixuan Chen <remch183@outlook.com>
2022-04-27 21:49:09 +0800

    fix: add logging for child.stderr #20

--- src/pure/runner.ts
+++ src/pure/runner.ts
@@ -3,6 +3,7 @@ import { readFile } from "fs-extra";
 import { tmpdir } from "os";
 import { existsSync } from "fs";
 import * as path from "path";
+import { Readable } from "stream";
 
 import { chunksToLinesAsync } from "@rauschma/stringio";
 import { sanitizeFilePath } from "./utils";
@@ -107,7 +108,12 @@ export class TestRunner {
         shell: isWindows,
       });
 
-      for await (const line of chunksToLinesAsync(child.stdout)) {
+      for await (
+        const line of mergeAsyncIter(
+          chunksToLinesAsync(child.stdout),
+          chunksToLinesAsync(child.stderr),
+        )
+      ) {
         log(line.trimEnd() + "\r\n");
         outputs.push(line);
       }
@@ -163,3 +169,45 @@ export async function getNodeVersion() {
     return line;
   }
 }
+
+const mergeAsyncIter = async function* <T>(
+  ...gens: AsyncIterable<T>[]
+): AsyncIterable<T> {
+  // Worker function to queue up the next result
+  const queueNext = async (
+    e: {
+      key: AsyncIterable<T>;
+      it: AsyncIterator<T>;
+      result?: IteratorResult<T, any> | null;
+    },
+  ) => {
+    e.result = null; // Release previous one as soon as possible
+    e.result = await e.it.next();
+    return e;
+  };
+  // Map the generators to source objects in a map, get and start their
+  // first iteration
+  const sources = new Map(gens.map((gen) => [
+    gen,
+    queueNext({
+      key: gen,
+      it: gen[Symbol.asyncIterator](),
+    }),
+  ]));
+  // While we still have any sources, race the current promise of
+  // the sources we have left
+  while (sources.size) {
+    const winner = await Promise.race(sources.values());
+    // Completed the sequence?
+    if (winner.result?.done) {
+      // Yes, drop it from sources
+      sources.delete(winner.key);
+    } else {
+      // No, grab the value to yield and queue up the next
+      // Then yield the value
+      const { value } = winner.result!;
+      sources.set(winner.key, queueNext(winner));
+      yield value;
+    }
+  }
+};
