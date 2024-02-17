c47127d04dfe507efd1f379fd47855a6237e37aa
Mathias Lorenzen <mathias.lorenzen@live.com>
2024-02-17 16:32:21 +0100

    fix: rename vite root config to avoid interference with local sample configs

--- vite.global-config.ts
+++ vite.global-config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
-    exclude: ['**/src/**', '**/node_modules/**', '**/samples/**', 'out/**'],
+    exclude: ['src/**', '**/node_modules/**', 'samples/**', 'out/**'],
   },
   base: './src/',
 })
