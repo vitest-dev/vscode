e31bd0069a82c9960897ba81f65dc8bbf957487c
Mathias <mathias.lykkegaard.lorenzen@lego.com>
2024-02-18 06:18:19 +0100

    fix: add using keyword test to basic sample

--- samples/basic/vite.config.ts
+++ samples/basic/vite.config.ts
@@ -2,11 +2,14 @@
 
 // Configure Vitest (https://vitest.dev/config)
 
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  test: {
-    include: ["src/should_included_test.ts", "test/**/*.test.ts"],
-    exclude: ["test/ignored.test.ts"],
+  esbuild: {
+    target: 'es2020',
   },
-});
+  test: {
+    include: ['src/should_included_test.ts', 'test/**/*.test.ts'],
+    exclude: ['test/ignored.test.ts'],
+  },
+})
