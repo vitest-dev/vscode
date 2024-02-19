744c99a4ded6c80e9c5eeb02b0f12f19a3b452d0
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-02-19 18:16:19 +0100

    chore: cleanup

--- src/api.ts
+++ src/api.ts
@@ -19,55 +19,62 @@ export interface BirpcEvents {
 
 type VitestAPI = BirpcReturn<BirpcMethods, BirpcEvents>
 
-export async function createVitestAPI(workspace: vscode.WorkspaceFolder) {
+function resolveVitestPath(workspace: vscode.WorkspaceFolder) {
   try {
-    const root = workspace.uri.fsPath
-    const vitestPath = require.resolve('vitest', { paths: [root] }) // resolves to cjs
-    const vitestNodePath = resolve(dirname(vitestPath), './dist/node.js')
-    log.info('[API]', 'Running Vitest from', vitestNodePath)
-    const worker = new Worker(workerPath, {
-      workerData: {
-        root,
-        vitestPath: vitestNodePath,
-      },
-    })
-    worker.stdout.on('data', (d) => {
-      log.info('[Worker]', d.toString())
-    })
-    worker.stderr.on('data', (d) => {
-      log.error('[Worker]', d.toString())
-    })
-
-    return await new Promise<VitestAPI>((resolve, reject) => {
-      const api = createBirpc<BirpcMethods, BirpcEvents>(
-        {
-          onReady() {
-            log.info('[API]', `Vitest for "${workspace.name}" workspace folder is resolved.`)
-            resolve(new Proxy(api, {
-              get(target, prop, receiver) {
-                if (prop === 'then')
-                  return undefined
-                return Reflect.get(target, prop, receiver)
-              },
-            }))
-          },
-          onError(err) {
-            reject(err)
-          },
-        },
-        {
-          on(listener) {
-            worker.on('message', listener)
-          },
-          post(message) {
-            worker.postMessage(message)
-          },
-        },
-      )
-    })
+    // resolves to unsupported cjs
+    const vitestPath = require.resolve('vitest', { paths: [workspace.uri.fsPath] })
+    return resolve(dirname(vitestPath), './dist/node.js')
   }
   catch (err: any) {
-    log.error('[API]', err?.stack)
+    log.info('[API]', `Vitest not found in "${workspace.name}" workspace folder`)
     return null
   }
+}
+
+export async function createVitestAPI(workspace: vscode.WorkspaceFolder) {
+  const vitestNodePath = resolveVitestPath(workspace)
+  if (!vitestNodePath)
+    return null
+  log.info('[API]', `Running Vitest for "${workspace.name}" workspace folder from ${vitestNodePath}`)
+  const worker = new Worker(workerPath, {
+    workerData: {
+      root: workspace.uri.fsPath,
+      vitestPath: vitestNodePath,
+    },
+  })
+  worker.stdout.on('data', (d) => {
+    log.info('[Worker]', d.toString())
+  })
+  worker.stderr.on('data', (d) => {
+    log.error('[Worker]', d.toString())
+  })
+
+  return await new Promise<VitestAPI | null>((resolve) => {
+    const api = createBirpc<BirpcMethods, BirpcEvents>(
+      {
+        onReady() {
+          log.info('[API]', `Vitest for "${workspace.name}" workspace folder is resolved.`)
+          resolve(new Proxy(api, {
+            get(target, prop, receiver) {
+              if (prop === 'then')
+                return undefined
+              return Reflect.get(target, prop, receiver)
+            },
+          }))
+        },
+        onError(err: any) {
+          log.error('[API]', err?.stack)
+          resolve(null)
+        },
+      },
+      {
+        on(listener) {
+          worker.on('message', listener)
+        },
+        post(message) {
+          worker.postMessage(message)
+        },
+      },
+    )
+  })
 }
