ed2bfac20cab30854e1ff9e0ecacda3e3c6fdec4
Yusuke Sakurai <kerokerokerop@gmail.com>
2024-02-20 00:06:26 +0900

    Update testName.ts

--- src/pure/testName.ts
+++ src/pure/testName.ts
@@ -1,6 +1,5 @@
-
 function escapeRegExp(str: string) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\\\^\|\.\$]/g, "\\$&");
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\\\^\|\.\$]/g, '\\$&')
 }
 
 const kReplacers = new Map<string, string>([
@@ -16,17 +15,19 @@ const kReplacers = new Map<string, string>([
 
 export function transformTestPattern(
   { testName, isEach }:
-    { testName: string; isEach: boolean }): string {
+  { testName: string; isEach: boolean },
+): string {
   // https://vitest.dev/api/#test-each
   // replace vitest's table test placeholder and treat it as regex
-  let result = testName;
+  let result = testName
   if (isEach) {
     // Replace object access patterns ($value, $obj.a) with %s first
     result = result.replace(/\$[a-zA-Z_.]+/g, '%s')
     result = escapeRegExp(result)
     // Replace percent placeholders with their respective regex
-    result = result.replace(/%[i#dfsjo%]/g, (m) => kReplacers.get(m) || m)
-  } else {
+    result = result.replace(/%[i#dfsjo%]/g, m => kReplacers.get(m) || m)
+  }
+  else {
     result = escapeRegExp(result)
   }
   return result
