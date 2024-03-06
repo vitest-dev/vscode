ed3158adf31cefa2c6f3f41e940c2ad680c6ac00
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-03-06 15:39:02 +0100

    chore: remove unused from the config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
-import semver from 'semver'
 import type { WorkspaceConfiguration, WorkspaceFolder } from 'vscode'
 import * as vscode from 'vscode'
-import { log } from './log'
-import { isDefinitelyVitestEnv, mayBeVitestEnv } from './pure/isVitestEnv'
-import { getVitestCommand, getVitestVersion, isNodeAvailable } from './pure/utils'
 
 export const extensionId = 'vitest.explorer'
 export const testControllerId = 'vitest'
@@ -46,89 +42,4 @@ export function getRootConfig() {
     changeBackgroundColor: rootConfig.get('changeBackgroundColor', true),
     disabledWorkspaceFolders: rootConfig.get<string[]>('disabledWorkspaceFolders', []),
   }
-}
-
-export const vitestEnvironmentFolders: ReadonlyArray<WorkspaceFolder> = []
-
-export async function detectVitestEnvironmentFolders() {
-  const vitestFolders = vitestEnvironmentFolders as WorkspaceFolder[]
-  vitestFolders.splice(0, vitestFolders.length)
-  if (
-    vscode.workspace.workspaceFolders == null
-    || vscode.workspace.workspaceFolders.length === 0
-  )
-    return
-
-  for (const folder of vscode.workspace.workspaceFolders) {
-    if ((await mayBeVitestEnv(folder) || getConfig(folder).enable) && !getRootConfig().disabledWorkspaceFolders.includes(folder.name))
-      vitestFolders.push(folder)
-  }
-}
-
-export interface VitestWorkspaceConfig {
-  workspace: vscode.WorkspaceFolder
-  isUsingVitestForSure: boolean
-  cmd: string
-  args: string[]
-  version?: string
-  isCompatible: boolean
-  isDisabled: boolean
-}
-
-export async function getVitestWorkspaceConfigs(): Promise<VitestWorkspaceConfig[]> {
-  return await Promise.all(vitestEnvironmentFolders.map(async (workspace) => {
-    const vscodeConfig = getConfig(workspace)
-    const cmd = getVitestCommand(workspace.uri.fsPath, vscodeConfig.commandLine)
-
-    const version = cmd == null
-      ? undefined
-      : await getVitestVersion(cmd, vscodeConfig.env || undefined, workspace.uri.fsPath).catch(async (e) => {
-        log.info(e.toString())
-        log.info(`process.env.PATH = ${process.env.PATH}`)
-        log.info(`vitest.nodeEnv = ${JSON.stringify(vscodeConfig.env)}`)
-        let errorMsg = e.toString()
-        if (!isNodeAvailable(vscodeConfig.env || undefined)) {
-          log.info('Cannot spawn node process')
-          errorMsg += 'Cannot spawn node process. Please try setting vitest.nodeEnv as {"PATH": "/path/to/node"} in your settings.'
-        }
-
-        log.error(errorMsg)
-        return undefined
-      })
-
-    const isUsingVitestForSure = (vscodeConfig.enable || await isDefinitelyVitestEnv(workspace) || (!!cmd)) && version != null
-    const disabled = getRootConfig().disabledWorkspaceFolders
-    const out: VitestWorkspaceConfig = cmd
-      ? {
-          workspace,
-          version,
-          isUsingVitestForSure,
-          cmd: cmd.cmd,
-          args: cmd.args,
-          isCompatible: isCompatibleVitestConfig({ version, workspace }),
-          isDisabled: disabled.includes(workspace.name),
-        }
-      : {
-          version,
-          workspace,
-          isUsingVitestForSure,
-          cmd: 'npx',
-          args: ['vitest'],
-          isCompatible: isCompatibleVitestConfig({ version, workspace }),
-          isDisabled: disabled.includes(workspace.name),
-        }
-    return out
-  }))
-}
-
-function isCompatibleVitestConfig(config: Pick<VitestWorkspaceConfig, 'version' | 'workspace'>) {
-  return !!((config.version && semver.gte(config.version, '0.12.0')) || getConfig(config.workspace).commandLine)
-}
-
-/**
- * @returns vitest workspaces filtered by `disabledWorkspaceFolders`
- */
-export function getValidWorkspaces(workspaces: vscode.WorkspaceFolder[]): vscode.WorkspaceFolder[] {
-  const { disabledWorkspaceFolders } = getRootConfig()
-  return workspaces.filter(workspace => !disabledWorkspaceFolders.includes(workspace.name)) || []
 }
