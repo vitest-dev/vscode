61f37d4b010084249e68c2d915497dab8090741b
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-02 19:52:40 +0200

    fix: improve vitest config glob

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,5 @@ export const minimumVersion = '1.4.0'
 export const distDir = resolve(__filename)
 export const workerPath = resolve(__dirname, 'worker.js')
 
-export const configGlob = '**/*{vite,vitest}*.config.{ts,js,mjs,cjs,cts,mts}'
+export const configGlob = '**/*{vite,vitest}*.config*.{ts,js,mjs,cjs,cts,mts}'
 export const workspaceGlob = '**/*vitest.{workspace,projects}*.{ts,js,mjs,cjs,cts,mts,json}'
