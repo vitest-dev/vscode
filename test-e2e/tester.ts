11a3c4d2470a21135c072d35ec036d23bf41d859
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-03 14:34:23 +0200

    chore: simplify test checks

--- test-e2e/tester.ts
+++ test-e2e/tester.ts
@@ -8,10 +8,8 @@ export class VSCodeTester {
 
   async openTestTab() {
     const tabLocator = this.page.getByRole('tab', { name: 'Testing' })
-    const handler = await tabLocator.elementHandle()
-    if (!handler)
-      throw new Error(`Cannot find locator: ${tabLocator}`)
-    if (await handler.getAttribute('aria-selected') !== 'true')
+    const attribute = await tabLocator.getAttribute('aria-selected')
+    if (attribute !== 'true')
       await tabLocator.locator('a').click()
   }
 
@@ -28,10 +26,7 @@ export class VSCodeTester {
     for (let i = 0; i < segments.length; i++) {
       const segment = segments[i]
       const locator = this.page.locator(`[aria-label*="${segment} "][aria-level="${i + 1}"]`)
-      const handler = await locator.elementHandle()
-      if (!handler)
-        throw new Error(`Cannot find locator: ${locator}`)
-      const state = await handler.getAttribute('aria-expanded')
+      const state = await locator.getAttribute('aria-expanded')
       if (state === 'true')
         continue
       await locator.click()
