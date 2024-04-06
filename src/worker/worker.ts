eec69c73493c14e97b9ec38f50086b2988721d37
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-06 16:22:38 +0200

    refactor: move reporter into its own file

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -1,82 +1,10 @@
 import v8 from 'node:v8'
 import { register } from 'node:module'
-import { nextTick } from 'node:process'
 import { dirname } from 'pathe'
-import { parseErrorStacktrace } from '@vitest/utils/source-map'
-import type { BirpcReturn } from 'birpc'
-import type { File, Reporter, TaskResultPack, UserConsoleLog, Vitest } from 'vitest'
-import type { BirpcEvents, BirpcMethods } from '../api/rpc'
 import { setupFilePath } from '../constants'
 import { createWorkerRPC } from './rpc'
 import type { WorkerMeta, WorkerRunnerOptions } from './types'
-
-class VSCodeReporter implements Reporter {
-  private rpc!: BirpcReturn<BirpcEvents, BirpcMethods>
-  private ctx!: Vitest
-  private id!: string
-
-  get isCollecting(): boolean {
-    return this.ctx.configOverride.testNamePattern?.toString() === '/$a/'
-  }
-
-  initVitest(ctx: Vitest, id: string) {
-    this.ctx = ctx
-    this.id = id
-    ctx.config.setupFiles = [
-      ...ctx.config.setupFiles || [],
-      setupFilePath,
-    ]
-    ctx.projects.forEach((project) => {
-      project.config.setupFiles = [
-        ...project.config.setupFiles || [],
-        setupFilePath,
-      ]
-    })
-  }
-
-  initRpc(rpc: BirpcReturn<BirpcEvents, BirpcMethods>) {
-    this.rpc = rpc
-  }
-
-  onUserConsoleLog(log: UserConsoleLog) {
-    this.rpc.onConsoleLog(this.id, log)
-  }
-
-  onTaskUpdate(packs: TaskResultPack[]) {
-    packs.forEach(([taskId, result]) => {
-      const project = this.ctx.getProjectByTaskId(taskId)
-
-      result?.errors?.forEach((error) => {
-        if (typeof error === 'object' && error) {
-          error.stacks = parseErrorStacktrace(error, {
-            getSourceMap: file => project.getBrowserSourceMapModuleById(file),
-          })
-        }
-      })
-    })
-
-    this.rpc.onTaskUpdate(this.id, packs)
-  }
-
-  onFinished(files?: File[], errors?: unknown[]) {
-    const collecting = this.isCollecting
-    nextTick(() => {
-      this.rpc.onFinished(this.id, files, errors, collecting)
-    })
-  }
-
-  onCollected(files?: File[]) {
-    this.rpc.onCollected(this.id, files, this.isCollecting)
-  }
-
-  onWatcherStart(files?: File[], errors?: unknown[]) {
-    this.rpc.onWatcherStart(this.id, files, errors, this.isCollecting)
-  }
-
-  onWatcherRerun(files: string[], trigger?: string) {
-    this.rpc.onWatcherRerun(this.id, files, trigger, this.isCollecting)
-  }
-}
+import { VSCodeReporter } from './reporter'
 
 async function initVitest(meta: WorkerMeta) {
   const vitestMode = await import(meta.vitestNodePath) as typeof import('vitest/node')
