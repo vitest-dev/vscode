d9abbbb83abe5fa02dafd8f71239a64e5fa2b64c
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-08 14:13:01 +0200

    chore: fix types

--- src/api/resolve.ts
+++ src/api/resolve.ts
@@ -37,7 +37,7 @@ export function resolveVitestPackage(cwd: string, folder: vscode.WorkspaceFolder
 }
 
 export function resolveVitestPackagePath(cwd: string, folder: vscode.WorkspaceFolder) {
-  const customPackagePath = getConfig(folder).packagePath
+  const customPackagePath = getConfig(folder).vitestPackagePath
   if (customPackagePath && !customPackagePath.endsWith('package.json'))
     throw new Error(`"vitest.packagePath" must point to a package.json file, instead got: ${customPackagePath}`)
   try {
