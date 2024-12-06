e911134b78d5d5064be7ae00293ab0879df73a41
Vladimir <sleuths.slews0s@icloud.com>
2024-12-06 09:39:08 +0100

    fix: support vitest beta (#551)

--- src/worker/watcher.ts
+++ src/worker/watcher.ts
@@ -15,7 +15,7 @@ export class VitestWatcher {
     // eslint-disable-next-line ts/no-this-alias
     const state = this
     const ctx = vitest.ctx
-    ;(ctx.getCoreWorkspaceProject().provide as <T extends keyof ProvidedContext>(key: T, value: ProvidedContext[T]) => void)('__vscode', {
+    ;(vitest.getRootTestProject().provide as <T extends keyof ProvidedContext>(key: T, value: ProvidedContext[T]) => void)('__vscode', {
       get continuousFiles() {
         return state.files || []
       },
