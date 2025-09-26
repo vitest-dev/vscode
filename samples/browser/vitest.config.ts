6d76cd55fb893d7ac2ee4e0cc5a02facccfccfb7
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-09-26 11:32:55 +0200

    chore: specify a different config for legacy tests

--- samples/browser/vitest.config.ts
+++ samples/browser/vitest.config.ts
@@ -3,22 +3,26 @@
 // Configure Vitest (https://vitest.dev/config)
 
 import { defineConfig } from 'vitest/config'
-import { playwright } from '@vitest/browser/providers/playwright'
 
-export default defineConfig({
-  esbuild: {
-    target: 'es2020',
-  },
-  test: {
-    include: ['test/**/*.test.ts'],
-    exclude: ['test/ignored.test.ts'],
-    browser: {
-      enabled: true,
-      headless: true,
-      provider: playwright(),
-      instances: [
-        { browser: 'chromium' },
-      ],
-    }
-  },
+export default defineConfig(async () => {
+  const provider: any = process.env.TEST_LEGACY !== 'true'
+    ? (await import('@vitest/browser/providers/playwright')).playwright()
+    : 'playwright'
+  return {
+    esbuild: {
+      target: 'es2020',
+    },
+    test: {
+      include: ['test/**/*.test.ts'],
+      exclude: ['test/ignored.test.ts'],
+      browser: {
+        enabled: true,
+        headless: true,
+        provider,
+        instances: [
+          { browser: 'chromium' },
+        ],
+      }
+    },
+  }
 })
