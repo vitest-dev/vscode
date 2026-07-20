01d22c6e15b625209c8f9e1d8ad26f178bfc93c3
Vladimir Sheremet <vsheremet@cloudflare.com>
2026-07-20 08:50:29 +0100

    chore: fix fmt

--- packages/extension/src/diagnostic.ts
+++ packages/extension/src/diagnostic.ts
@@ -15,7 +15,7 @@ export class ExtensionDiagnostic {
         error.message.toString(),
         vscode.DiagnosticSeverity.Error,
       )
-      diagnostic.source = 'Vitest';
+      diagnostic.source = 'Vitest'
       diagnostics.push(diagnostic)
     })
     this.diagnostic.set(testFile, diagnostics)
